b0402663444ce14df5d707ae175b0e6422515249
Taylor Hobbs <taylor@feemon.com>
2015-05-20 07:58:13 -0500

    Inner join query

---
+++ people/join-tests.sql
@@ -0,0 +1,36 @@
+psql -d people
+
+-- Insert values to test against
+INSERT INTO job (name)
+    VALUES  ('Engineer'),
+            ('Manager');
+
+INSERT INTO person (first_name, last_name, job)
+    VALUES  ('John', 'Smith', 'Manager'),
+            ('Dave', 'Smith', 'Engineer');
+
+-- Begin tests
+BEGIN;
+    SELECT plan(2);
+
+    SELECT fk_ok('person', 'job', 'job', 'name');
+
+    PREPARE inner_join_have AS SELECT person.first_name, job.NAME
+                                   FROM
+                                    person person INNER JOIN job job
+                                   ON
+                                    person.job = job.NAME
+                                   WHERE
+                                    person.job = 'Manager';
+
+    PREPARE inner_join_want AS VALUES ('John', 'Manager');
+
+    SELECT results_eq('inner_join_have', 'inner_join_want', 'Correctly got John Manager');
+
+
+-- Finish tests and clear table
+SELECT * FROM finish();
+ROLLBACK;
+
+DELETE FROM person *;
+DELETE FROM job *;
